0d30dab8fb24bcddbcf644051f3aa09d07176ca4
andradeju <julliana.andrade08@gmail.com>
2024-06-30 20:27:32 -0300

    :sparkles: feat: Add localStorage e função de tirar de foto de reconhecimento facial

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@ import eUmCPF from "./validaCpf.js";
 import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposFormularios = document.querySelectorAll('[required]');
+const formulario = document.querySelector("[data-formulario]");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const listaRespostas = {
+    "nome": e.target.elements["nome"].value,
+    "email": e.target.elements["email"].value,
+    "rg": e.target.elements["rg"].value,
+    "cpf": e.target.elements["cpf"].value,
+    "aniversario": e.target.elements["aniversario"].value,
+
+  }
+  localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+  window.location.href = '../pages/abrir-conta-form-2.html';
+
+})
 
 camposFormularios.forEach((campo) => {
   campo.addEventListener("blur", () => verificaCampo(campo))
@@ -49,7 +67,7 @@ const mensagens = {
 
 function verificaCampo(campo) {
   let mensagem = "";
-  campo.setCustomValidity('');
+  campo.setCustomValidity(''); 
   if(campo.name == "cpf" && campo.value.length >= 11) {
     eUmCPF(campo);
 
